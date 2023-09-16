53f752c049c343477f0b72be2f9ee8043d404eda
Makar Mikhalchenko <makar.mikhalchenko@bk.ru>
2023-09-16 17:57:59 +0300

    Changed project structure, edited webpack configs

--- app/Http/Controllers/SPA/SPAController.php
+++ app/Http/Controllers/SPA/SPAController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\SPA;
 
 use App\Http\Controllers\Controller;
+use Illuminate\View\View;
 
 class SPAController extends Controller
 {
-    public function index()
+    public function index(): View
     {
-        return view('main');
+        $manifestPath = public_path('/build/asset-manifest.json');
+
+        if (file_exists($manifestPath)) {
+            return view('main', ['jsFile' => 'build' . json_decode(file_get_contents($manifestPath), true)['files']['main.js']]);
+        }
+
+        return view('main', ['jsFile' => 'build/static/js/fallback.js']);
     }
 }
