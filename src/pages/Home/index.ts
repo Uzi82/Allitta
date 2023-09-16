c8bc865100dd1eb4d231a58ac5dd7c8f63c95cbc
Makar Mikhalchenko <makar.mikhalchenko@bk.ru>
2023-09-16 19:58:11 +0300

    Delete old folder

--- src/pages/Home/index.ts
+++
@@ -1,5 +0,0 @@
-export { ProductCard } from "../../components/ProductCard"
-export { FlexCenter } from "../../UI/FlexCenter"
-export { Header } from "../../components/Header"
-export { Footer } from "../../components/Footer" 
-export { Home } from './UI/'
