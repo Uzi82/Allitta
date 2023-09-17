6d3e0d0ef9f8cf47feaaf262f3db9622b68cb1ac
Uzi <gssdsudas@gmail.com>
2023-09-17 12:54:48 +0400

    Footer 90%

--- frontend/src/components/Footer/UI/styled.ts
+++ frontend/src/components/Footer/UI/styled.ts
@@ -19,9 +19,65 @@ export const FooterContent = styled.div`
 export const Contact = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 30px;
+    gap: 20px;
 `
 
 export const ContactText = styled.div`
-    /* Will continue later */
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+
+export const ContactHeader = styled.div`
+    margin: 10px 0;
+    color: #5D20D6;
+    font-family: Inter-Bold, sans-serif;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+`
+
+export const ContactDescription = styled.div`
+    color: #FFF;
+    width: 262px;
+    font-family: Inter-Regular, sans-serif;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+`
+
+export const PagesCategories = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+
+export const PagesCategoriesHeader = styled.div`
+    color: #5D20D6;
+    font-family: Inter-Bold, sans-serif;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    margin: 10px 0;
+`
+
+export const PagesCategoriesElement = styled.div`
+    color: #FFF;
+    font-family: Inter-Regular, sans-serif;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+`
+export const EmailDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin: 20px 0;
+`
+
+export const EmailInput = styled.input`
+    padding: 3px 40px 3px 10px;
 `
