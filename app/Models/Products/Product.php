8999bd3352fcf3e9b844822d5216791e42d3902d
Makar Mikhalchenko <makar.mikhalchenko@bk.ru>
2023-09-22 01:58:28 +0300

    feat: Add full-text search for products by name

--- app/Models/Products/Product.php
+++ app/Models/Products/Product.php
@@ -2,10 +2,21 @@
 
 namespace App\Models\Products;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function scopeSearchByName(Builder $query, string $name): Builder
+    {
+        if ($name) {
+            $tsQuery = implode(':* & ', explode(' ', trim($name))) . ':*';
+            $query->whereRaw('name_tsvector @@ to_tsquery(?)', $tsQuery);
+        }
+
+        return $query;
+    }
 }
