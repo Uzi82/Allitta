149f20119cbf4f18db0ac8a4412457500beb43c9
sifes <lonfark@gmail.com>
2023-09-24 09:13:23 +0300

    initialize react-hook-form

--- frontend/src/components/SignUp/UI/CreateCustomerAccount.tsx
+++ frontend/src/components/SignUp/UI/CreateCustomerAccount.tsx
@@ -1,37 +1,55 @@
 import React from 'react'
-import { Flex, Subtitle, Title, Wrapper } from './styled'
+import { Flex, Select, Subtitle, Title, FormWrapper } from './styled'
 import { Input } from '../../../UI/Input'
 import { CustomLink } from '../../../UI/CustomLink'
-import { useParams } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
 
-const CreateCustomerAccount: React.FC = () => {
-    const s = useParams()
+interface ICreateCustomer {
+    firstName: string,
+    lastName: string,
+    fullName: string,
+    date: typeof Date,
+    gender: 'not-specified' | 'male' | 'female' | 'own-gender',
+    nic: number,
+    tel: number,
+    address: string,
+    city: string,
+    zip: number,
+}
+
+export const CreateCustomerAccount: React.FC = () => {
+    const { } = useForm<ICreateCustomer>({
+        defaultValues: {}
+    })
     return (
-        <Wrapper>
+        <FormWrapper>
             <Title $mb='30px'>Your Profile</Title>
             <Subtitle $mb='12px'>Your Informations</Subtitle>
             <Flex $mb='20px'>
-                <Input placeholder='First Name' />
-                <Input placeholder='Last Name' />
+                <Input placeholder='First Name' type='text' />
+                <Input placeholder='Last Name' type='text' />
             </Flex>
-            <Input mb='20px' width='100%' placeholder='Full Name' />
+            <Input mb='20px' placeholder='Full Name' type='text' />
             <Flex $mb='20px'>
-                <Input placeholder='Birthday (DD/MM/YYYY)' />
-                <Input placeholder='Gender' />
+                <Input placeholder='Birthday (DD/MM/YYYY)' type='date' />
+                <Select placeholder='Gender' >
+                    <option value="not-specified">Not Specified</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="own-gender">Own Gender</option>
+                </Select>
             </Flex>
             <Flex $mb='30px'>
-                <Input placeholder='NIC Number' />
-                <Input placeholder='Mobile Number' />
+                <Input placeholder='NIC Number' type='number' />
+                <Input placeholder='Mobile Number' type='tel' />
             </Flex>
             <Subtitle $mb='20px'>Home Address</Subtitle>
-            <Input mb='20px' width='100%' placeholder='Street Address' />
+            <Input mb='20px' placeholder='Street Address' />
             <Flex $mb='40px'>
-                <Input placeholder='City' />
-                <Input placeholder='Zip Code' />
+                <Input placeholder='City' type='text' />
+                <Input placeholder='Zip Code' type='number' />
             </Flex>
             <CustomLink to='/signup/customer/photo'>Continue</CustomLink>
-        </Wrapper>
+        </FormWrapper>
     )
 }
-
-export { CreateCustomerAccount }
--- frontend/src/pages/routes.tsx
+++ frontend/src/pages/routes.tsx
@@ -1,11 +1,37 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Home } from './Home/'
-import { SignInPage } from './SignIn'
+import { SignUpPage } from './SignUpPage'
+import { SignInPage } from './SignInPage'
+import { CreateCustomerAccount, CreateShoperAccount, ProfilePicture, Verification } from '../components/SignUp'
 
 export const publicRoutes = createBrowserRouter([
     {
         path: '/',
         element: <Home />
+    },
+    {
+        path: '/signup',
+        element: <SignUpPage />,
+        children: [
+            {
+                index: true,
+                element: <CreateShoperAccount />,
+            },
+            {
+                path: '/signup/verify',
+                element: <Verification />,
+            },
+            {
+                path: '/signup/customer',
+                element: <CreateCustomerAccount />,
+            },
+            {
+                path: '/signup/customer/photo',
+                element: <ProfilePicture />,
+            },
+        ]
+
+
     },
     {
         path: '/signin',
