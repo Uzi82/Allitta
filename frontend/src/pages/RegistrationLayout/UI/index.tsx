eeeefefc435dfcd04deb595f3f38430b3e269853
sifes <lonfark@gmail.com>
2023-10-03 19:20:41 +0300

    refact BackgroundBlur
    add $isShown flag

--- frontend/src/pages/RegistrationLayout/UI/index.tsx
+++ frontend/src/pages/RegistrationLayout/UI/index.tsx
@@ -5,7 +5,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 export const RegistrationLayout: React.FC = () => {
     return (
-        <BackgroundBlur circles close logo >
+        <BackgroundBlur $isShown circles close logo >
             <Outlet></Outlet>
             <ToastContainer position="top-center" autoClose={2000} limit={2} newestOnTop closeOnClick rtl={false} pauseOnFocusLoss draggable theme="dark" />
         </BackgroundBlur>
