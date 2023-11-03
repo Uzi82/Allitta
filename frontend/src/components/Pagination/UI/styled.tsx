c562010cf5b77884f3d2f270989f257ce629b790
sifes <lonfark@gmail.com>
2023-11-03 21:14:26 +0200

    feat: pagination component

---
+++ frontend/src/components/Pagination/UI/styled.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const PaginationContainer = styled.div`
+`;
+
+export const PageEllipsis = styled.span`
+  padding: 8px;
+  margin: 2px;
+  user-select: none;
+  width: 35px;
+  height: 35px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  margin: 0 5px;
+  background-color: #E6E6E6;
+  border-radius: 5px;
+  user-select: none;
+`;
