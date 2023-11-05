4f46faf0c220096977ed8a80606e86b48008a8cb
sifes <lonfark@gmail.com>
2023-11-05 19:35:42 +0200

    feat: Filters set up

---
+++ frontend/src/modules/HomeProducts/UI/Filters/Filters.styled.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+`
+export const Header = styled.div`
+`
+export const Title = styled.h2`
+`
+export const ButtonReset = styled.button`
+`
+export const Divider = styled.div`
+`
+export const PriceWrapper = styled.div`
+`
+export const PriceInput = styled.input`
+`
+export const MaxPriceTitle = styled.p`
+`
+export const PriceSelect = styled.select`
+`
+export const RadioGroup = styled.div`
+`
+export const RadioGroupHeader = styled.div`
+`
+export const RadioGroupTitle = styled.h3`
+`
+export const RadioGroupExpand = styled.div`
+`
+export const RadioGroupElement = styled.input`
+`
