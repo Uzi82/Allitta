91c024b7e04d2faceb6f0c6ac11ea438ad07de90
sifes <lonfark@gmail.com>
2023-11-07 20:27:28 +0200

    feat: create products

--- frontend/src/modules/HomeProducts/UI/index.tsx
+++ frontend/src/modules/HomeProducts/UI/index.tsx
@@ -1,12 +1,15 @@
 import { useQuery } from "react-query"
-import { Content, Main, MainTitle, MainText, ProductsList } from "./styled"
+import { Content, Main, MainTitle, MainText, ProductsList, Wrapper, Card, Text, ImageBackground, Header, Price, Desctiption } from "./styled"
 import { getFilteredProducts } from "../models/getFilteredProducts"
 import { Pagination } from "../../../components/Pagination"
 import React from "react"
 import { Search } from "./Search"
 import { Filters } from "./Filters"
+import { Button } from "../../../UI/Button"
+import { useNavigate } from "react-router-dom"
 
 export const HomeProducts: React.FC = () => {
+    const navigate = useNavigate()
     const [page, setPage] = React.useState(1)
     const { data, isLoading, isError } = useQuery('getFilteredProducts', getFilteredProducts, {
         refetchOnWindowFocus: false
@@ -16,7 +19,7 @@ export const HomeProducts: React.FC = () => {
         return <div>no products</div>
     }
     return (
-        <Content>
+        <Wrapper>
             <Main>
                 <MainTitle>Shop Online</MainTitle>
                 <MainText>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</MainText>
@@ -25,12 +28,23 @@ export const HomeProducts: React.FC = () => {
             <Content>
                 <Filters />
                 <ProductsList>
-                    {data.map((product) => (
-                        <div key={product.id}>{product.name}</div>
-                    ))}
+                    {data.length ? data.map(({ cost, description, id, name, currency, logotype_path }) => (
+                        <Card key={id}>
+                            <ImageBackground src={logotype_path} />
+                            <Text>
+                                <Header>{name}</Header>
+                                <Price>{cost}</Price>
+                                <Desctiption>{description}</Desctiption>
+                                <Button width='116px' height='35px' mt={'15px'} onClick={() => navigate(`/products/${id}`)}>Add to cart</Button>
+                            </Text>
+                        </Card>
+                    ))
+                        :
+                        <div>no products</div>
+                    }
                 </ProductsList>
             </Content>
             <Pagination count={7} onChange={setPage} />
-        </Content>
+        </Wrapper>
     )
 }
--- frontend/src/modules/HomeProducts/UI/styled.ts
+++ frontend/src/modules/HomeProducts/UI/styled.ts
@@ -1,18 +1,24 @@
 import styled from 'styled-components'
 const searchBg = require('../assets/bgSearch.png')
 
-export const Content = styled.div`
+export const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
-    width: 1108px;
     gap: 30px;    
     @media screen and (min-width: 767px) and (max-width: 1120px) {
-        width: calc(100vw - 20px);
         gap: 10px; 
     }
     @media screen and (max-width: 766px) {
         gap: 10px;
-        width: calc(100vw - 20px);
+    }
+`
+export const Content = styled.div`
+    display: flex;
+    gap: 30px;    
+    @media (max-width: 768px) {
+        flex-direction: column;
+        gap: 20px;
+        align-items: center;
     }
 `
 
@@ -36,6 +42,20 @@ export const Main = styled.div`
     }
 `
 export const ProductsList = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-column-gap: 18px;
+    grid-row-gap: 28px;
+    width: 100%;
+    @media screen and (max-width: 900px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+    @media screen and (max-width: 768px) {
+        grid-template-columns: repeat(3, 1fr);
+    }
+    @media screen and (max-width: 480px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
 `
 export const MainTitle = styled.h1`
     font-family: Inter-Regular, sans-serif;
@@ -54,4 +74,75 @@ export const MainText = styled.div`
     text-align: center;
     font-size: 14px;
     margin-bottom: 20px;
+`
+
+
+
+export const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    max-width: 262px;
+    @media screen and (min-width: 767px) and (max-width: 1120px) {
+        gap: 5px;
+    }
+    @media screen and (max-width: 766px) {
+        gap: 5px;
+    }
+`
+export const ImageBackground = styled.div<{ src: string }>`
+    width: 100%;
+    background-image: url(${props => props.src});
+    max-width: 262px;
+    min-height: 1px;
+    padding-top: 100%;
+    border-radius: 10px;
+    background: #D9D9D9;
+`
+
+export const Text = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const Header = styled.h1`
+    font-family: Inter-Bold, sans-serif;
+    font-size: 18px;
+    word-break: break-all;
+    padding: 0 2px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    @media screen and (min-width: 767px) and (max-width: 1120px) {
+        font-size: 14px; 
+        width: calc(20vw - 20px);
+    }
+`
+
+export const Price = styled.p`
+    font-family: Inter-Bold, sans-serif;
+    padding: 0 2px;
+    margin: 5px 0;
+    word-break: break-all;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    @media screen and (min-width: 767px) and (max-width: 1120px) {
+        font-size: 12px; 
+    }
+`
+
+export const Desctiption = styled.p`
+    font-family: Inter-Regular, sans-serif;
+    font-size: 14px;
+    word-break: break-all;
+    padding: 0 2px;
+    margin: 5px 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    @media screen and (min-width: 767px) and (max-width: 1120px) {
+        font-size: 12px;
+        margin: 0;
+    }
 `
